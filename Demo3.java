68854eaad27208f7dc67699ee97bd8028db6fc30
reddypurushothm@gmail.com <reddypurushothm@gmail.com>
2026-01-30 00:13:24 -1200

    Arrays

--- Demo3.java
+++ Demo3.java
@@ -5,15 +5,15 @@ public class Demo3 {
         Scanner scan=new Scanner(System.in);
         int [][] a =new int [2][5] ;
         int i,j;
-        for (i = 0; i <2; i++) {
-            for (j = 0; j <5; j++) {
+        for (i = 0; i <a.length; i++) {
+            for (j = 0; j <a[i].length; j++) {
                 System.out.println("enter your ages "+"class "+i+" student "+j);
                 a[i][j]=scan.nextInt();
                 
             }
         }
-             for (i = 0; i <2; i++) {
-                    for (j = 0; j <5; j++) {
+             for (i = 0; i <a.length; i++) {
+                    for (j = 0; j <a[i].length; j++) {
                         System.out.print(a[i][j]+" ");
                     }
                      System.out.println();
