1066429752f32807c82a9cd5c19ecc68ae6d952f
reddypurushothm@gmail.com <reddypurushothm@gmail.com>
2026-01-29 22:19:39 -1200

    Arrays

---
+++ Input.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+      System.err.println("Here we are doing additio");
+      System.out.println("enter first value");
+      int a= scan.nextInt();
+      System.out.println("enter second value");
+      int b=scan.nextInt();
+      System.out.println("you will get output of a+b");
+      int c=a+b;
+      System.out.println(c);
+    }
+    
+}
