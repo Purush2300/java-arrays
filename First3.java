30d2b97358dd836ca5a41e44bad32c880eb93822
reddypurushothm@gmail.com <reddypurushothm@gmail.com>
2026-01-31 23:41:05 -1200

    code

--- First3.java
+++ First3.java
@@ -8,18 +8,30 @@ public class First3 {
         a[i]=scan.nextInt();
         
     }
-    int count=0;
-    int max=a[0];
-    int min=a[0];
-     for (int i = 0; i < a.length-1; i++) {
-            for (int j = 1; j < a.length-1; j++) {
-                count+=a[j];
-                System.out.println(count);
-
+   int count=0;
+   int max=Integer.MIN_VALUE;
+   int min=Integer.MAX_VALUE;
+     for (int i = 0; i < a.length; i++) {
+        count+=a[i];
+           
+    }
+    for (int i = 0; i < a.length; i++) {
+        if(a[i]>max){
+            max=a[i];
+        }
         
-            }
+    }
+     for (int i = 0; i < a.length; i++) {
+        if(a[i]<min){
+            min=a[i];
+        }
         
-      }
+    }
+    System.out.println(count);
+    System.out.println(max);
+    System.out.println(min);
+    System.out.println(count-min+" this is max value");
+    System.out.println(count-max+" this is min value");
     
     
 
