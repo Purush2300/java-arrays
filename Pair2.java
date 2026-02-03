8ff36889b9631432fcd52644e94500dc08540f4f
reddypurushothm@gmail.com <reddypurushothm@gmail.com>
2026-02-02 21:11:04 -1200

    Arrays

---
+++ Pair2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Pair2 {
+    public static void main(String[] args) {
+        Scanner scan =new Scanner(System.in);
+        int n=scan.nextInt();
+        int [] a=new int [n];
+        for (int i = 0; i < a.length; i++) {
+            a[i]=scan.nextInt();
+        }
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i+1; j < a.length; j++) {
+                System.out.print("["+a[i]+","+a[j]+"]");
+                
+            }
+            System.out.println();
+        }
+    }
+    
+}
